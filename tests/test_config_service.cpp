011797b7491d5b7f18b6cb52c9bbc7e232c7ca5f
mrmees <mattmees@gmail.com>
2026-07-26 21:22:39 -0500

    test(aa): align GAL 5.0 default coverage

--- tests/test_config_service.cpp
+++ tests/test_config_service.cpp
@@ -23,7 +23,7 @@ void TestConfigService::testReadTopLevelValues()
     QCOMPARE(svc.value("display.brightness").toInt(), 80);
     QCOMPARE(svc.value("audio.master_volume").toInt(), 80);
     QCOMPARE(svc.value("connection.wifi_ap.ssid").toString(), QString("OpenAutoProdigy"));
-    QCOMPARE(svc.value("connection.gal_version").toString(), QString("4.3"));
+    QCOMPARE(svc.value("connection.gal_version").toString(), QString("5.0"));
     QCOMPARE(svc.value("video.fps").toInt(), 30);
     QCOMPARE(svc.value("hardware_profile").toString(), QString("rpi4"));
     QCOMPARE(svc.value("api.tcp_port").toInt(), 9810);
